01b13aa21ee875773929cac913efcb047f50f083
Zayd Nubani <zayd.nubani@gmail.com>
2022-08-03 17:16:29 -0400

    popup error

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,38 +1,24 @@
-// const popup = document.getElementsByClassName('popup');
+wind = window.location.href.split('/');
+const popBtn = document.getElementById('popBtn');
 
-// if(window.location.href.indexOf('flora')>-1){
+popBtn.addEventListener('click',() => {
+    window.localStorage.setItem('popped', 'true');
+})
 
-//     for(let i=0; i< popup.length; i++){
-
-
-//         const nft = document.getElementsByClassName('nft');
-
-//         // popup[i].children[0].addEventListener('click', () => {
-
-//             popup[i].style = 'opacity: 0; top: -250%; transform: translate(-50%, -50%); left: 50%;';
+const bunk = () => {
+    wind = window.localStorage.getItem('popped')
+    const popup = document.getElementsByClassName('popup')
     
-//             for( let i=0; i<nft.length; i++ ){
+    for(let i = 0; i < popup.length; i++){
+        if (wind === 'true'){
+            console.log('true');
+            popup[i].style = 'display: none'
+    
+        } else {
+            console.log('false')
+            popup[i].style = 'top: 50%; left: 50%; transform: translate(-50%, -50%);'
+        }
+    }
+}
 
-//                 if(nft[i].className === 'container direct-col align-center justify-center text red w-25 nft membership'){
-//                     console.log('member');
-//                     nft[i].style =`opacity: 1; order: ${Math.floor(Math.random() * 4)};`
-
-//                 } else {
-
-//                     nft[i].style =`opacity: 1; order: ${Math.floor(Math.random() * 27)};`
-
-//                 }
-            
-//             };
-//         // });
-//     }
-// } else {
-
-//     for(let i=0; i<popup.length; i++){
-
-//         popup[i].style = 'display:none;';
-
-//     }
-// }
-
-
+window.onload = bunk()
