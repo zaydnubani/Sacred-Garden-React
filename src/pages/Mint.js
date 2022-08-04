f2b0312b3f298fa985c5ce386629b564d5c67ac3
Zayd Nubani <zayd.nubani@gmail.com>
2022-08-04 14:12:40 -0400

    public

--- src/pages/Mint.js
+++ src/pages/Mint.js
@@ -9,10 +9,11 @@ import initWeb3Onboard from '../services';
 import { DeltaFloraGenesis__factory } from '../ethers-contracts';
 import { ethers } from 'ethers';
 import toast, { Toaster } from 'react-hot-toast';
-import { TESTNET_LEAVES_DATA } from '../emarald/SG_leaves.js';
-import MerkleTree from 'merkletreejs';
-import keccak256 from 'keccak256';
-
+// for greenList mint
+// import { TESTNET_LEAVES_DATA } from '../emarald/SG_leaves.js';
+// import MerkleTree from 'merkletreejs';
+// import keccak256 from 'keccak256';
+import { WinterCheckout } from '@usewinter/checkout'
 
 const MAINNET_CONTRACT_ADDRESS = "0x21374d22f169849cfd680241f3f37cd61ac2eea5";
 
@@ -142,55 +143,91 @@ const Mint = () => {
     })
   }
 
-  const sendMint = async (amount) => {
-    function hash(account) {
-      return Buffer.from(
-        ethers.utils.solidityKeccak256(
-          ['address'],
-          [account]
-        ).slice(2), 'hex');
-    }
+  // const sendMint = async (amount) => {
+  //   function hash(account) {
+  //     return Buffer.from(
+  //       ethers.utils.solidityKeccak256(
+  //         ['address'],
+  //         [account]
+  //       ).slice(2), 'hex');
+  //   }
     
-    // retrieves the wallet providers address
-    const address = wallet.accounts[0].address;
-    if (provider && wallet?.accounts[0]){
-      try { 
-        console.log('Trying to transact')
-        // This connects the contract to the wallet provider to initialize communication and pass information
-        const sacrdgardn = DeltaFloraGenesis__factory.connect(MAINNET_CONTRACT_ADDRESS, provider.getSigner());
-        // Returns a promise to await a response from the contract to send it's mintPrice
-        const stage = await sacrdgardn.currentStage();
-        const pricePer = (await sacrdgardn.stages(stage))[2];
+  //   // retrieves the wallet providers address
+  //   const address = wallet.accounts[0].address;
+  //   if (provider && wallet?.accounts[0]){
+  //     try { 
+  //       console.log('Trying to transact')
+  //       // This connects the contract to the wallet provider to initialize communication and pass information
+  //       const sacrdgardn = DeltaFloraGenesis__factory.connect(MAINNET_CONTRACT_ADDRESS, provider.getSigner());
+  //       // Returns a promise to await a response from the contract to send it's mintPrice
+  //       const stage = await sacrdgardn.currentStage();
+  //       const pricePer = (await sacrdgardn.stages(stage))[2];
 
-        const leaves = MerkleTree.unmarshalLeaves(TESTNET_LEAVES_DATA);
-        const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
-        const proof = tree.getHexProof(hash(address));
-        console.log("PROOF", proof);
-        console.log('quanity', amount);
-        console.log('address', address);
-        console.log('price', (pricePer.mul(amount)).toString())
+  //       const leaves = MerkleTree.unmarshalLeaves(TESTNET_LEAVES_DATA);
+  //       const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+  //       const proof = tree.getHexProof(hash(address));
+  //       console.log("PROOF", proof);
+  //       console.log('quanity', amount);
+  //       console.log('address', address);
+  //       console.log('price', (pricePer.mul(amount)).toString())
 
-        await sacrdgardn.allowlistMint(
-          address,
-          amount,
-          proof,
-            // proof, *** proof is to verify if they are on greenlist
-          { value: pricePer.mul(amount) }
-        ).then((event) => {
-          console.log(event)
+  //       await sacrdgardn.allowlistMint(
+  //         address,
+  //         amount,
+  //         proof,
+  //           // proof, *** proof is to verify if they are on greenlist
+  //         { value: pricePer.mul(amount) }
+  //       ).then((event) => {
+  //         console.log(event)
           
-        });
-      } catch (err) {
-          // console.log(err)
-          throw new Error("Transaction failed to initiate.");
+  //       });
+  //     } catch (err) {
+  //         // console.log(err)
+  //         throw new Error("Transaction failed to initiate.");
       
-        }
-      return true;
+  //       }
+  //     return true;
+  //   }
+  //   else {
+  //     return false;
+  //   }
+  // }
+
+  const sendMint = async (amount) => {
+      // retrieves the wallet providers address
+      const address = wallet.accounts[0].address;
+      if (provider && wallet?.accounts[0]){
+        try { 
+          console.log('Trying to transact')
+          // This connects the contract to the wallet provider to initialize communication and pass information
+          const sacrdgardn = DeltaFloraGenesis__factory.connect(MAINNET_CONTRACT_ADDRESS, provider.getSigner());
+          // Returns a promise to await a response from the contract to send it's mintPrice
+          const stage = await sacrdgardn.currentStage();
+          const pricePer = (await sacrdgardn.stages(stage))[2];
+  
+          console.log('quanity', amount);
+          console.log('address', address);
+          console.log('price', (pricePer.mul(amount)).toString())
+  
+          await sacrdgardn.publicMint(
+            address,
+            amount,
+            { value: pricePer.mul(amount) }
+          ).then((event) => {
+            console.log(event)
+            
+          });
+        } catch (err) {
+            // console.log(err)
+            throw new Error("Transaction failed to initiate.");
+        
+          }
+        return true;
+      }
+      else {
+        return false;
+      }
     }
-    else {
-      return false;
-    }
-  }
 
   
   const inc = () => {
@@ -220,9 +257,37 @@ const Mint = () => {
       </div>
     )
   }
+
   
-  
-  const PrePurchase = () => {
+    const PrePurchase = () => {
+
+      const [showModal, setModal] = useState();
+
+      useEffect(()=>{
+        const winter = document.getElementById('winter')
+        winter.addEventListener('click', () =>{setModal(true)})
+        return window.removeEventListener('click', () =>{setModal(true)})
+      },[])
+
+      function handleWindowEvent(event) {
+        if (event.data === "closeWinterCheckoutModal") {
+            // properly close the winter modal so it can be opened again
+            setModal(false)
+        } else if (event.data.name === 'successfulWinterCheckout') {
+        // Successfully checked out. This event contains information 
+        // you can use to continue the flow: 
+        console.log(event.data.transactionhash) // do what you need with the txhash here! 
+        console.log(event.data.email) // email that the user bought an NFT with
+        setComplete(true)
+        }
+      }
+
+      useEffect(() => {
+          window.addEventListener("message", handleWindowEvent)
+          return () => window.removeEventListener("message", handleWindowEvent)
+      }, [])
+
+
       return (
       <div className='container direct-col space-evenly align-center w-100 bg-p-bot bg-s-100 bg-norepeat p-5' style={{backgroundImage: `url('${mint_2100}')`}}>
         <Toaster />
@@ -255,11 +320,22 @@ const Mint = () => {
           <button className='btn text solid-red p-1 m-2 mint-button br-5 w-75' 
           onClick={() => (wallet? handleMint(quantity) : connect())}
           // onClick={() => (wallet? null : connect())}
-
           >
             {connecting ? 'CONNECTING' : wallet ? 'MINT FLORA' : 'CONNECT WALLET'}
           </button>
         </div>
+        <div className='container direct-col space-evenly align-center w-50'>
+          <button id='winter' className='btn text solid-red p-1 m-2 mint-button br-5 w-75'>Mint with card</button>
+        </div>
+        <WinterCheckout 
+            projectId={6858} 
+            production={true} 
+            showModal={showModal} 
+            // Extra mint params are params besides 'address, amount, proof'
+            // The key needs to exactly match the name of the param provided to Winter
+            // The value will be passed in as the param
+            extraMintParams={{a: 1, b: 2}}
+        />
       </div>
     )
   }
